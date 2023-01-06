2b611f6492e8dfb2cadb3d4b9582af844cde1ec2
V_Pelizzari <115237214+ViniciusPelizzari@users.noreply.github.com>
2023-01-06 16:14:45 -0300

    Version 1.0

--- HelloWord0.java
+++ HelloWord0.java
@@ -2,10 +2,7 @@ public class HelloWord0 {
 
     public static void main(String[] args) {
         System.out.print("Hello Word!\nMy name is Vinicius Pelizzari da Silva, i have 19 years.\n");
-        // System.out.print("My name is Vinicius Pelizzari da Silva, i have 19
-        // years.\n");
         System.out.print("My dream is to be developer java.\nToday is my first day development in language Java!\n");
-        // System.out.print("Today is my first day development in language Java!\n");
     }
 }
 // Developer VINÍCIUS PELIZARI
