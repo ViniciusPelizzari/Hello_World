847b83ae53fe1b11acb917d5e00a3e4ca1210a8a
V_Pelizzari <vinipelizzari2410@gmail.com>
2023-01-09 10:49:24 -0300

    Add files via upload

---
+++ teste1.java
@@ -0,0 +1,10 @@
+public class teste1 {
+
+    public static void main(String[] args) {
+        Object var1 = "Hello World!";
+        Object var2 = "Olá mundo!";
+
+        System.out.println(var1.equals(var2));
+    }
+}
+// Developer VINÍCIUS PELIZARI
